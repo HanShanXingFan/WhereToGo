894e01a36e5bea746f6ec3f61e37b8feed982ebb
HanShanXingFan <937258842@qq.com>
2018-09-29 00:03:22 +0800

    添加图标轮播

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,6 +1,17 @@
 <template>
 	<div class="icons">
-		icons
+		<swiper :options="swiperOption">
+			<swiper-slide v-for="(icon, index) in pages" :key="index">
+			<div class="icon" v-for="item in icon" :key="item.id">
+				<div class="icon-img">
+					<img class='icon-img-content' :src="item.imgUrl" alt="" />
+				</div>
+				<p class="icon-desc">{{item.desc}}</p>
+			</div>
+			</swiper-slide>
+		</swiper>
+		
+
 	</div>
 </template>
 
@@ -9,16 +20,108 @@
 		name: 'icons',
 		data () {
 			return {
-				
+				swiperOption: {
+					
+				},
+				iconList: [
+					{
+						id: '0001',
+						imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '景点门票'
+					},
+					{
+						id: '0002',
+						imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+						desc: '一日游'
+					},
+					{
+						id: '0003',
+						imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+						desc: '上海必备'
+					},
+					{
+						id: '0004',
+						imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+						desc: '游乐场'
+					},
+					{
+						id: '0005',
+						imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ea/01d081dacb03cc02.png',
+						desc: '赏秋色'
+					},
+					{
+						id: '0006',
+						imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/57/1e29afd06f881102.png',
+						desc: '普陀山'
+					},
+					{
+						id: '0007',
+						imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+						desc: '欢乐谷'
+					},
+					{
+						id: '0008',
+						imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+						desc: '野生动物园'
+					},
+					{
+						id: '0009',
+						imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/de/f26be47a6bf02a02.png',
+						desc: '上海迪士尼'
+					}
+				]
+			}
+		},
+		computed: {
+			pages () {
+				const pages = []
+				this.iconList.forEach((item, index) => {  //把一位数组拆解为二维数组
+					const page = Math.floor(index/8) //页码,如index为2 2/8向下取整为0,0是第一个页面
+					if (!pages[page]) {
+						pages[page] = []
+						console.log(11)
+					}
+					pages[page].push(item)
+				})
+				return pages
 			}
 		}
 	}
 </script>
 
 <style lang="stylus" scoped>
-	.icons
-		overflow: hidden
+	@import '~styles/varibles.styl'
+	@import '~styles/mixins.styl'
+	.icons >>> .swiper-container
 		height: 0
 		padding-bottom: 50%
-		background: skyblue
+	.icon
+		position: relative
+		overflow: hidden
+		float: left
+		width: 25%
+		height: 0
+		padding-bottom: 25%
+		.icon-img
+			position: absolute
+			top: 0
+			left: 0
+			right: 0
+			bottom: .44rem
+			box-sizing: border-box
+			padding: .1rem
+			.icon-img-content
+				display: block
+				margin: 0 auto
+				height: 100%
+		.icon-desc
+			position: absolute
+			left: 0
+			right: 0
+			bottom: 0
+			height: .44rem
+			line-height: .44rem
+			color: $darkTextColor
+			text-align: center
+			ellipsis()
 </style>
