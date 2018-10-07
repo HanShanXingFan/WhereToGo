f5dfef3f502ca22049cb94b2adfceec00787ee82
HanShanXingFan <937258842@qq.com>
2018-10-07 23:23:02 +0800

    add search to city

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -41,7 +41,7 @@
 				res = res.data
 				if (res.ret && res.data) {
 					const data = res.data
-					console.log(data)
+//					console.log(data)
 					this.city = data.city
 					this.swiperList = data.swiperList
 					this.iconList = data.iconList
