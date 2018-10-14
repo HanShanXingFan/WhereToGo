e2fb093445f96f7d094fcd2c03c1b0e7859daeb1
HanShanXingFan <937258842@qq.com>
2018-10-14 22:36:54 +0800

    完成keep-alive缓存点击不同城市在发送请求,相同城市不发请求

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@
 	import HomeIcons from './components/Icons'
 	import HomeRecommend from './components/Recommend'
 	import HomeWeekend from './components/Weekend'
+	import { mapState } from 'vuex'
 	export default {
 		name: 'home',
 		components: {
@@ -26,15 +27,19 @@
 		},
 		data () {
 			return {
+				lastCity: '', //初次进入页面所显示的城市
 				swiperList: [],
 				iconList: [],
 				recommendList: [],
 				weekendList: []
 			}
 		},
+		computed: {
+			...mapState(['city'])
+		},
 		methods: {
 			getHomeInfo () {
-				this.$http.get('/api/index.json').then(this.getHomeInfoSucc)
+				this.$http.get('/api/index.json?city='+ this.city).then(this.getHomeInfoSucc)
 			},
 			getHomeInfoSucc (res) {
 				res = res.data
@@ -49,7 +54,17 @@
 			}
 		},
 		mounted () {
+//			console.log('mounted')
+			this.lastCity = this.city
 			this.getHomeInfo()
+		},
+		activated () {
+			//当前的页面的城市和上一次页面显示的城市是否一样,一样不发请求,不一样发请求
+			if (this.lastCity != this.city) {
+				this.lastCity = this.city
+				this.getHomeInfo()
+			}
+//			console.log('activated')
 		}
 	}
 </script>
