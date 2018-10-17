5dca496603f33650a2b1e9ab4dfd46d30c48060e
HanShanXingFan <937258842@qq.com>
2018-10-17 23:45:39 +0800

    vue滚动头部渐隐渐现

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,15 +1,19 @@
 <template>
 	<div class="detail">
 		<detail-banner></detail-banner>
+		<detail-header></detail-header>
+		<div class="content"></div>
 	</div>
 </template>
 
 <script>
 	import DetailBanner from './components/Banner'
+	import DetailHeader from './components/Header'
 	export default {
 		name: 'detail',
 		components: {
-			DetailBanner
+			DetailBanner,
+			DetailHeader
 		},
 		data () {
 			return {
@@ -20,5 +24,6 @@
 </script>
 
 <style lang="stylus" scoped>
-
+	.content
+		height: 20rem
 </style>
